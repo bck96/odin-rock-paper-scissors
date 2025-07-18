f5e6e18a6dbb376b0d980623015ca4bba9975bb9
bck96 <168590151+bck96@users.noreply.github.com>
2025-07-18 13:28:04 -0400

    Added full game functionality, display messages

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,29 +1,108 @@
 playerScore = 0;
 cpuScore = 0;
 
+playGame();
+
+// Generate the CPU's choice
 function getCpuChoice(){
-    return Math.floor(Math.random() * 3) + 1;
+    let rng = Math.floor(Math.random() * 3) + 1;
+    if (rng === 1){
+        cpuChoice = "rock";
+    }
+    else if (rng === 2){
+        cpuChoice = "paper";
+    }
+    else if (rng === 3){
+        cpuChoice = "scissors";
+    }
+    console.log("CPU Choice: " + capitalizeFirstLetter(cpuChoice));
+    return cpuChoice;
 }
 
-getCpuChoice();
-console.log(getCpuChoice());
-
+// Input the player's choice
 function getPlayerChoice(){
-    let choice = prompt("Make your choice: Rock, Paper, or Scissors.");
-    console.log(choice);
+    let choice = prompt("Make your choice: Rock, Paper, or Scissors.").toLowerCase();
+    console.log("Player Choice: " + capitalizeFirstLetter(choice));
     return choice;
 }
 
-getPlayerChoice();
+// Play a single round
+function playRound(playerChoice, cpuChoice){
+    if (playerChoice === cpuChoice){
+        showTie(playerChoice);
+    }
+    else{
+        if (playerChoice === "rock"){
+            if (cpuChoice === "paper"){
+                showResults("lose", playerChoice, cpuChoice);
+            }
+            else if (cpuChoice === "scissors"){
+                showResults("win", playerChoice, cpuChoice);
+            }
+        }
+        else if (playerChoice === "paper"){
+            if (cpuChoice === "rock"){
+                showResults("win", playerChoice, cpuChoice);
+            }
+            else if (cpuChoice === "scissors"){
+                showResults("lose", playerChoice, cpuChoice);
+            }
+        }
+        else if (playerChoice === "scissors"){
+            if (cpuChoice === "rock"){
+                showResults("lose", playerChoice, cpuChoice);
+            }
+            else if (cpuChoice === "paper"){
+                showResults("win", playerChoice, cpuChoice);
+            }
+        }
+    }
+}
 
-// Get computer choice
-    // Randomly return "rock", "paper", or "scissors"
+// Player a full game of 5 rounds
+function playGame(){
+    for (i = 1; i < 6; i++){
+        console.log("Round " + i + " of 5. START!")
+        playRound(getPlayerChoice(), getCpuChoice())
+        console.log("Player Score: " + playerScore);
+        console.log("CPU Score: " + cpuScore);
 
-// Get human choice (use prompt)
+        if (i === 5){
+            if (playerScore > cpuScore){
+                console.log("You win! Congratulations!")
+            }
+            else if (playerScore < cpuScore){
+                console.log("You lose! Better luck next time.")
+            }
+            else{
+                console.log("Tie! You're evenly matched.")
+            }
 
-// Track player and computer scores
+            console.log("Refresh to try again.")
+        }
+    }
+}
 
-// Play single round
-    // Increment score of winner
+// Display results if not a tie
+function showResults(winLose, playerChoice, cpuChoice){
+    resultsMessage = "You " + winLose + "!";
+    if (winLose === "win"){
+        comparisonMessage = capitalizeFirstLetter(playerChoice + " beats " + cpuChoice);
+        playerScore++;
+    }
+    else if (winLose === "lose"){
+        comparisonMessage = capitalizeFirstLetter(cpuChoice + " beats " + playerChoice);
+        cpuScore++;
+    }
+    console.log(resultsMessage + " " + comparisonMessage + ".")
+}
 
-// Player five rounds
+// Display result of a tie
+function showTie(choice){
+    console.log("Tie! You both chose " + choice + ".");
+}
+
+// Capitalize the first letter of a string
+function capitalizeFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
